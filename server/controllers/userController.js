2a6969872cd213459898e7547e55a4207420ab1f
Fruiticecake <1157279689@qq.com>
2022-11-06 19:16:06 +0800

    用户接口的注册功能

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,16 +5,48 @@ const db = require("../config/db");
  */
 
 exports.registerController = (req, res) => {
-  const sql =
-    //'insert into user (name,pwd,head_img) value ("ceshi","666666","https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/default/head_img/18.jpeg");';
-    'SELECT * FROM course.user;'
-  db.query(sql, (err, result) => {
+  //用户名或密码判空
+  let { userName, password } = req.body;
+  if (!userName || !password) {
+    return res.send({ code: 1, message: "用户名或者密码不能为空！" });
+  }
+  const userSelectSql = "select * from user where name=?";
+  db.query(userSelectSql, userName, (err, results) => {
     if (err) {
       return res.send({ code: 1, message: err.message });
     }
-    res.send({
-        code:0,
-        data:'注册成功',
-    });
+    //判断是否存在用户
+    console.log(results);
+    if (results.length > 0) {
+      return res.send({ code: 1, message: "该用户已经存在" });
+    }
+    //用户密码加密
+    const bcrypt = require("bcryptjs");
+    const passwordB = bcrypt.hashSync(password, 10);
+    //生成随机头像
+    const imgList = [
+      "https://github.com/Fruiticecake/images/blob/main/course-images/%E5%A4%B4%E5%83%8F%20%E5%A5%B3%E5%AD%A9%20(1).png",
+      "https://github.com/Fruiticecake/images/blob/main/course-images/%E5%A4%B4%E5%83%8F%20%E5%A5%B3%E5%AD%A9%20(2).png",
+      "https://github.com/Fruiticecake/images/blob/main/course-images/%E5%A4%B4%E5%83%8F%20%E5%A5%B3%E5%AD%A9.png",
+      "https://github.com/Fruiticecake/images/blob/main/course-images/%E5%A4%B4%E5%83%8F%20%E7%94%B7%E5%AD%A9.png",
+      "https://github.com/Fruiticecake/images/blob/main/course-images/%E5%A4%B4%E5%83%8F%20%E7%94%B7%E5%AD%A9%20(1).png",
+      "https://github.com/Fruiticecake/images/blob/main/course-images/%E5%A4%B4%E5%83%8F%20%E7%94%B7%E5%AD%A9%20(2).png",
+    ];
+    const num = Math.floor(Math.random() * 6 + 1);
+    //用户信息插入
+    const userInsertSql = "insert into user (name,pwd,head_img) value (?,?,?)";
+    db.query(
+      userInsertSql,
+      [userName, passwordB, imgList[num]],
+      (err, results) => {
+        if (err) {
+          return res.send({ code: 1, message: err.message });
+        }
+        res.send({
+          code: 0,
+          message: "注册成功",
+        });
+      }
+    );
   });
 };
