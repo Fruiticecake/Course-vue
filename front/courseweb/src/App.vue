0539d05af8356b17e789f83ba0bbdd9b20795e12
Fruiticecake <1157279689@qq.com>
2022-11-08 00:52:10 +0800

    vue初始化

---
+++ front/courseweb/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style></style>
